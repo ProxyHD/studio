7fd273b10cc1f26063d2003ad685a707226973c1
Rodrigo27p _YT <rodrigo09pinho@gmail.com>
2025-09-10 13:07:10 +0000

    olha demora muito a caregar a pagina quando abro para outra pagina explo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,9 +22,7 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
       </head>
       <body className={cn("min-h-screen bg-background font-body antialiased")}>
-        <RootLayoutClient>
-          {children}
-        </RootLayoutClient>
+        <RootLayoutClient>{children}</RootLayoutClient>
         <Toaster />
       </body>
     </html>
